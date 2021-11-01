5fbcd71cc40b34845c28dd7eecd20fe05cfd412c
Michasssss <michaeldzindzio@gmail.com>
2021-11-01 21:33:01 +0100

    Initial commit

---
+++ Models/TreeNode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZadanieRekrutacyjneIdeo.Models
+{
+    public class TreeNode
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int ID { get; set; }
+
+        [Display(Name = "Name")]
+        public int Name { get; set; }
+
+        [Display(Name = "ParentID")]
+        public int? PID { get; set; }
+
+        [ForeignKey("PID")]
+        public TreeNode Parent { get; set; }
+
+        public ICollection<TreeNode> Childs { get; set; }
+    }
+}
